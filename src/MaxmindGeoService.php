7902881639da871817fa63b1afe06370dd8f7fb1
yuriy <yuralucky83@gmail.com>
2021-05-08 21:05:44 +0300

    first commit

---
+++ src/MaxmindGeoService.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Hillel\Maxmind;
+
+
+use GeoIp2\Database\Reader;
+
+class MaxmindGeoService implements GeoService
+{
+    protected $reader;
+
+    protected $data;
+
+    public function __construct()
+    {
+        $this->reader =
+            new Reader(base_path() . DIRECTORY_SEPARATOR .
+                'database' . DIRECTORY_SEPARATOR . 'geo' . DIRECTORY_SEPARATOR . 'geoip.mmdb');
+
+    }
+
+    public function parse($ip)
+    {
+        $this->data = $this->reader->country($ip);
+    }
+
+    public function continentCode()
+    {
+        return $this->data->continent->code;
+    }
+
+    public function countryCode()
+    {
+        return $this->data->country->isoCode;
+    }
+}
